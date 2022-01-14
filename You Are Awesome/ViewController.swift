8b4a713dc1fdf6bb4f6f5893b6f267bc7745a764
Krista Pickens <kpickens@zagmail.gonzaga.edu>
2022-01-14 14:39:20 -0800

    Initial Commit

---
+++ You Are Awesome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  You Are Awesome
+//
+//  Created by Krista Pickens on 1/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
