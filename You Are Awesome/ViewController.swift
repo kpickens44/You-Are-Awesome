151098b1491d9401b22f1cf9f40e444874603195
Krista Pickens <kpickens@zagmail.gonzaga.edu>
2022-01-20 12:00:49 -0800

    2.8 Completed If/ else if

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -14,13 +14,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     
+        
     }
-
+    
     @IBAction func msgButtonPressed(_ sender: UIButton) {
-        msgLabel.text = "You Are Awesome!"
-        imageView.image = UIImage(named: "image1")
+        let awesomemsg = "You Are Awesome!"
+        let greatmsg = "You Are Great!"
+        let bombmsg = "You Are Da Bomb!"
+        
+        
+        if msgLabel.text == awesomemsg {
+            msgLabel.text = greatmsg
+            imageView.image = UIImage(named: "image2")
+        } else if msgLabel.text == greatmsg {
+            msgLabel.text = bombmsg
+            imageView.image = UIImage(named: "image3")
+        } else {
+            msgLabel.text = awesomemsg
+            imageView.image = UIImage(named: "image1")
+        }
     }
     
 }
-
